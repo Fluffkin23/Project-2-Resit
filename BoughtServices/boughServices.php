f8c30148dc5c5fa1b96ad37bd67dbf42e40cca3d
Rares-001 <91318114+Rares-001@users.noreply.github.com>
2023-01-11 13:31:33 +0100

    Update boughServices.php
    added boughtservices

--- BoughtServices/boughServices.php
+++ BoughtServices/boughServices.php
@@ -1,1 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar">
+
+        <div class="logo">
+            <img src="/res/logo.png" alt="logo">
+        </div>
+
+        <ul class="nav-links">
+
+            <div class="menu">
+                <li><a href="/Contract/contracts.html">Contracts</a></li>
+                <li><a href="/Services/services.html">Services</a></li>
+                <li><a href="/Ticket/ticket.html">Ticket</a></li>
+                <li><a href="/contactsUs.html">Contact Us</a></li>
+                <li><a href="/login/login.html">Log In</a></li>
+            </div>
+
+        </ul>
+    </nav>
+
+    <div class="footer">
+        <h1>The future of change: are you ready?</h1>
+    </div>
+</body>
+
+</html>
