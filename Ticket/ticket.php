7e3d1c45d80d1c986c506165420d233712c1f19a
Rares-001 <91318114+Rares-001@users.noreply.github.com>
2023-01-16 19:18:50 +0100

    minor fixes

--- Ticket/ticket.php
+++ Ticket/ticket.php
@@ -21,15 +21,19 @@ require_once("connectionDB.php");
         <select name="services" id="services">
 
             <?php
+            if (isset($_SESSION['EMAIL']) && $_SESSION['PASSWORD']['loggedIn']) {
+                // good to show protected content
+            
 
-
-        $sql = "SELECT SERVICE_NAME FROM request_services";
-        $result = mysqli_query($connection, $sql);
-        while ($row = mysqli_fetch_assoc($result)) {
-            $id = $row['SERVICE_NAME'];
-            $service = $row['SERVICE_NAME'];
-            echo "<option value='$id'>$service</option>";
-        }
+                $sql = "SELECT SERVICE_NAME FROM request_services" . $_SESSION['EMAIL'];
+                ;
+                $result = mysqli_query($connection, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $id = $row['SERVICE_NAME'];
+                    $service = $row['SERVICE_NAME'];
+                    echo "<option value='$id'>$service</option>";
+                }
+            }
         ?>
 
         </select>
@@ -40,48 +44,44 @@ require_once("connectionDB.php");
 
     <?php
 
+    if (isset($_SESSION['EMAIL']) && $_SESSION['PASSWORD']['loggedIn']) {
+        // good to show protected content
+    
 
+        if (isset($_POST['submit'])) {
+            $serviceName = $_POST['services'];
+            $description = $_POST['description'];
 
-    if (isset($_POST['submit'])) {
-        $serviceName = $_POST['services'];
-        $description = $_POST['description'];
-
-        $sql = "INSERT INTO ticket_table (SERVICE_NAME, SERVICE_DESCRIPTION) VALUES ('$serviceName', '$description')" or die(mysqli_error($db));
-        $result = mysqli_query($connection, $sql) or trigger_error("Query Failed! SQL: $sql - Error: " . mysqli_error($connection), E_USER_ERROR);
-        if (!$result) {
-            die('Query FAILED' . mysqli_error($connection));
-        } else {
-            echo '<script type="text/javascript">
+            $sql = "INSERT INTO ticket_table (SERVICE_NAME, SERVICE_DESCRIPTION) VALUES ('$serviceName', '$description')" or die(mysqli_error($db)) . $_SESSION['EMAIL'];
+            ;
+            $result = mysqli_query($connection, $sql) or trigger_error("Query Failed! SQL: $sql - Error: " . mysqli_error($connection), E_USER_ERROR);
+            if (!$result) {
+                die('Query FAILED' . mysqli_error($connection));
+            } else {
+                echo '<script type="text/javascript">
             window.onload = function () { alert("Record created!"); } 
         </script>';
+            }
+        }
+
+
+
+        //get results from database
+        $result = mysqli_query($connection, "SELECT SERVICE_NAME, SERVICE_DESCRIPTION, DATE, STATUS FROM ticket_table");
+        $all_property = array(); //declare an array for saving property
+    
+
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo "Service name: " . $row["SERVICE_NAME"] . " - Service description: " . $row["SERVICE_DESCRIPTION"] . " - Date " . $row["DATE"] . " - Status " . $row["STATUS"] . "<br>";
+            }
+        } else {
+            echo "0 results";
         }
     }
-
-
-
-    //get results from database
-    $result = mysqli_query($connection, "SELECT * FROM ticket_table");
-    $all_property = array(); //declare an array for saving property
-
-    //showing property
-    echo '<table class="data-table">
-        <tr class="data-heading">'; //initialize table tag
-            while ($property = mysqli_fetch_field($result)) {
-            echo '<td class=tdStyle>' . $property->name . '</td>'; //get field name for header
-            $all_property[] = $property->name; //save those to array
-            }
-            echo '</tr>'; //end tr tag
-
-        //showing all data
-        while ($row = mysqli_fetch_array($result)) {
-        echo "<tr>";
-            foreach ($all_property as $item) {
-            echo '<td>' . $row[$item] . '</td>'; //get items using property value
-            }
-            echo '</tr>';
-        }
-        echo "
-    </table>";
+$connection->close();
     ?>
 </body>
 
